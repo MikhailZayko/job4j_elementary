6d3beee2e32b5ee994075d93b7bcd22dc3ecdba0
Admin <mishavolgodonsk@gmail.com>
2023-09-14 23:04:23 +0300

    3. Перегрузить метод max для трех чисел [#122644]

--- src/test/java/ru/job4j/condition/MaxTest.java
+++ src/test/java/ru/job4j/condition/MaxTest.java
@@ -7,37 +7,58 @@ import static org.assertj.core.api.Assertions.assertThat;
 class MaxTest {
     @Test
     void whenMax1To2Then2() {
-        int left = 1;
-        int right = 2;
-        int result = Max.max(left, right);
+        int first = 1;
+        int second = 2;
+        int result = Max.max(first, second);
         int expected = 2;
         assertThat(result).isEqualTo(expected);
     }
 
     @Test
     void whenMax3To2Then3() {
-        int left = 3;
-        int right = 2;
-        int result = Max.max(left, right);
+        int first = 3;
+        int second = 2;
+        int result = Max.max(first, second);
         int expected = 3;
         assertThat(result).isEqualTo(expected);
     }
 
     @Test
     void whenMax3To5Then5() {
-        int left = 3;
-        int right = 5;
-        int result = Max.max(left, right);
+        int first = 3;
+        int second = 5;
+        int result = Max.max(first, second);
         int expected = 5;
         assertThat(result).isEqualTo(expected);
     }
 
     @Test
     void whenMax6To6Then6() {
-        int left = 6;
-        int right = 6;
-        int result = Max.max(left, right);
+        int first = 6;
+        int second = 6;
+        int result = Max.max(first, second);
         int expected = 6;
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    void whenMax4To5To6Then6() {
+        int first = 4;
+        int second = 5;
+        int third = 6;
+        int result = Max.max(first, second, third);
+        int expected = 6;
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    void whenMax5To5To6To9Then9() {
+        int first = 5;
+        int second = 5;
+        int third = 6;
+        int fourth = 9;
+        int result = Max.max(first, second, third, fourth);
+        int expected = 9;
         assertThat(result).isEqualTo(expected);
     }
 }
